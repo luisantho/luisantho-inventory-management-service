0997057f385ceb97ba1793f30ab7973bda35a662
Luis Anthony <anthonytoapanta15@gmail.com>
2025-06-24 10:10:59 +0200

    WIP Category API

--- src/main/java/com/luisanthony/portfolio/inventory_management_service/controller/CategoryController.java
+++ src/main/java/com/luisanthony/portfolio/inventory_management_service/controller/CategoryController.java
@@ -3,6 +3,9 @@ package com.luisanthony.portfolio.inventory_management_service.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +23,19 @@ public class CategoryController {
     @Autowired
     private CategoryService service;
 
-    @PostMapping(path = "")
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategoryById (@PathVariable Long id) {
+        return new ResponseEntity<>(service.getCategoryById(id),HttpStatus.OK);
+    }
+
+    @PostMapping("")
     public ResponseEntity<Category> create(@Valid @RequestBody Category category) {
         return new ResponseEntity<>(service.createCategory(category), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable Long id) {
+        service.deleteCategory(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/com/luisanthony/portfolio/inventory_management_service/service/CategoryService.java
+++ src/main/java/com/luisanthony/portfolio/inventory_management_service/service/CategoryService.java
@@ -3,8 +3,11 @@ package com.luisanthony.portfolio.inventory_management_service.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.luisanthony.portfolio.inventory_management_service.exception.ResourceNotFoundException;
 import com.luisanthony.portfolio.inventory_management_service.model.Category;
 import com.luisanthony.portfolio.inventory_management_service.repository.CategoryRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class CategoryService {
@@ -12,8 +15,21 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
+    public Category getCategoryById(Long id) {
+        return repository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("Category not found by id " + id));
+    }
+
+    @Transactional
     public Category createCategory(Category category) {
         return repository.save(category);
     }
-    
+
+    @Transactional
+    public void deleteCategory (Long id) {
+        if (!repository.existsById(id)) { //SELECT COUNT(*)
+            throw new ResourceNotFoundException("Category not found with ID: " + id);
+        }
+        repository.deleteById(id);
+    }
 }
